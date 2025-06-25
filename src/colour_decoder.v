2ba767caeb1a0ca04a60583dcfcdf1c4c5d64af5
noahhu22 <145583254+noahhu22@users.noreply.github.com>
2025-06-24 20:09:48 -0400

    Update and rename colour_encoder_t.v to colour_decoder.v

--- src/colour_decoder.v
+++ src/colour_decoder.v
@@ -3,13 +3,13 @@
 
 module colour_decoder (
     input  wire [3:0] ui,      
-    output wire [1:0] colour   
+    output wire [1:0] colour_dec_out   
 );
 
     // colour[1] goes high for Yellow or Green
-    assign colour[1] = ui[2] | ui[3];
+    assign colour_dec_out[1] = ui[2] | ui[3];
 
     // colour[0] goes high for Blue or Green
-    assign colour[0] = ui[1] | ui[3];
+    assign colour_dec_out[0] = ui[1] | ui[3];
 
 endmodule
