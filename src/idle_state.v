18ffb975fa2e6d3cff4226795967752198f83030
miche <michelleskater28@gmail.com>
2025-07-04 20:23:43 -0400

    attempt 1 to integrate w/ lfsr

--- src/idle_state.v
+++ src/idle_state.v
@@ -3,48 +3,46 @@ module IDLE_STATE(
     input wire en_IDLE,
     input wire rst_IDLE,
     input wire complete_LFSR,
-    input wire [6:0] LFSR_output,
-    output wire en_LFSR,
-    output wire [6:0] 32b_MEM_IN,
-    output wire 32b_MEM_LOAD,
-    output wire complete_IDLE
-    output wire 32b_MEM_LOAD_VAL
-)
+    input wire [7:0] LFSR_output,
+    output reg en_LFSR,
+    output reg [7:0] MEM_IN,
+    output reg MEM_LOAD,
+    output reg complete_IDLE,
+    output reg [1:0]MEM_LOAD_VAL
+);
 
-wire [1:0] count = 2'b00; 
+reg [1:0] count;
 
-always @(posedge clk)begin
-    if(rst_IDLE)
+always @(posedge clk) begin
+    if (rst_IDLE) begin
         en_LFSR <= 1'b0;
-        32b_MEM_IN <= 32'b0;
-        32b_MEM_LOAD <= 1'b0;
+        MEM_IN <= 8'b0;
+        MEM_LOAD <= 1'b0;
         complete_IDLE <= 1'b0;
         count <= 2'b00;
-    else if(en_IDLE) 
-        if(complete_LFSR && ~32b_MEM_LOAD) // lfsr is done shifting (ie valid value on lfsr output), and has not been loaded to mem.
-            begin
-                en_LFSR <= 1'b0; // disable LFSR to stop further shifting
-                32b_MEM_IN <= LFSR_output; // load the lfsr output into the mem
-                32b_MEM_LOAD_VAL <= count; // set the load value to current count
-                32b_MEM_LOAD <= 1'b1; // enable mem load
-            end
-        else if(32b_MEM_LOAD) begin
-            // may need to reset lfsr counter ??
-            en_LFSR <= 1'b1; // reenable lfsr 
-            32b_MEM_LOAD <= 1'b0; // reset mem load signal
-            if(count == 2'b00) count <= 2'b01; // increment count
-            else if(count == 2'b01) count <= 2'b10; // increment count
-            else if(count == 2'b10) count <= 2'b11; // increment count
-            else if(count == 2'b11) begin
-                complete_IDLE <= 1'b1; // set complete signal when all bits loaded
-                count <= 2'b00; // reset count for next cycle
-            end else begin
-                complete_IDLE <= 1'b0; // reset complete signal if not done
-        end
-        else begin
-            en_LFSR <= 1'b1; // keep LFSR enabled for next cycle
-        end
+    end else if (en_IDLE) begin
+        if (complete_LFSR && ~MEM_LOAD) begin
+            en_LFSR <= 1'b0;
+            MEM_IN <= LFSR_output;
+            MEM_LOAD_VAL <= count;
+            MEM_LOAD <= 1'b1;
+        end else if (MEM_LOAD) begin
+            en_LFSR <= 1'b1;
+            MEM_LOAD <= 1'b0;
 
-            
+            if (count == 2'b00) count <= 2'b01;
+            else if (count == 2'b01) count <= 2'b10;
+            else if (count == 2'b10) count <= 2'b11;
+            else if (count == 2'b11) begin
+                complete_IDLE <= 1'b1;
+                count <= 2'b00;
+            end else begin
+                complete_IDLE <= 1'b0;
+            end // <- missing END added here!
+        end else begin
+            en_LFSR <= 1'b1;
+        end
+    end
+end
 
 endmodule
