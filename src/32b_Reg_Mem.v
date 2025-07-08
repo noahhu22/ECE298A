aa0cbd5f5b5ffdaae91f6b9b501c56ea6bfc6462
miche <michelleskater28@gmail.com>
2025-07-07 21:22:25 -0400

    attempt 1 MEM integration

--- src/32b_Reg_Mem.v
+++ src/32b_Reg_Mem.v
@@ -1,34 +1,24 @@
-// remove dff module when merge w/ lfsr code, already defined there.
-module dff(output reg q, input d, input clk, input en, input rst);
-    always @(posedge clk or posedge rst) begin
-        if (rst)
-            q <= 1'b0;
-        else if (en)
-            q <= d;
-    end
-endmodule
-
 // 32-bit Register Memory w/ async reset signal.
-module 32b_Reg_Mem(
+module MEM(
     input wire clk,
-    input wire 32b_MEM_LOAD,
-    input wire [7:0] 32b_MEM_IN,
-    input wire rst_32b_MEM,
-    input wire [1:0] 32b_MEM_LOAD_VAL, // which bits to load.
-    output wire [31:0] 32b_MEM_OUT
+    input wire MEM_LOAD,
+    input wire [7:0] MEM_IN,
+    input wire rst_MEM,
+    input wire [1:0] MEM_LOAD_VAL, // which bits to load.
+    output reg[31:0] MEM_OUT
 );
 
-@always (posedge clk)begin
-    if(rst_32b_MEM)
-        32b_MEM_OUT  <= 32'b00000000000000000000000000000000;
-    else if(32b_MEM_LOAD_VAL == 2'b00 && 32b_MEM_LOAD)
-        32b_MEM_OUT <= {32b_MEM_OUT[31:8], 32b_MEM_IN[7:0]}
-    else if(32b_MEM_LOAD_VAL == 2'b01 && 32b_MEM_LOAD)
-        32b_MEM_OUT <= {32b_MEM_OUT[31:16], 32b_MEM_IN[7:0], 32b_MEM_OUT[7:0]}
-    else if(32b_MEM_LOAD_VAL == 2'b10 && 32b_MEM_LOAD)
-        32b_MEM_OUT <= {32b_MEM_OUT[31:24], 32b_MEM_IN[7:0], 32b_MEM_OUT[15:0]}
-    else if(32b_MEM_LOAD_VAL == 2'11 && 32b_MEM_LOAD)
-        32b_MEM_OUT <= {32b_MEM_IN[7:0], 32b_MEM_OUT[23:0]}
-
-
+always @(posedge clk)begin
+    if(rst_MEM)begin
+        MEM_OUT  <= 32'b00000000000000000000000000000000;
+    end else if(MEM_LOAD_VAL == 2'b00 && MEM_LOAD) begin
+        MEM_OUT <= {MEM_OUT[31:8], MEM_IN[7:0]};
+    end else if(MEM_LOAD_VAL == 2'b01 && MEM_LOAD) begin
+        MEM_OUT <= {MEM_OUT[31:16], MEM_IN[7:0], MEM_OUT[7:0]};
+    end else if(MEM_LOAD_VAL == 2'b10 && MEM_LOAD) begin
+        MEM_OUT <= {MEM_OUT[31:24], MEM_IN[7:0], MEM_OUT[15:0]};
+    end else if(MEM_LOAD_VAL == 2'b11 && MEM_LOAD)begin
+        MEM_OUT <= {MEM_IN[7:0], MEM_OUT[23:0]};
+    end 
+    end
 endmodule
